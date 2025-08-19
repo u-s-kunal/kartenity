c822b06fc630f83eecbc5f58cf9db139b098ac0a
kunal <developerkunal01@gmail.com>
2025-08-19 20:40:01 +0530

    fixed minor bugs

--- app/pages/products/ProductList.jsx
+++ app/pages/products/ProductList.jsx
@@ -133,7 +133,7 @@ function ProductList() {
 
                 {product.image && (
                   <img
-                    src={`http://localhost:5000${product.image}`}
+                    src={`${process.env.NEXT_PUBLIC_API_URL}${product.image}`}
                     alt={product.name}
                     className="w-full h-40 object-contain"
                   />
